f7a9d02c6bb39c772616c396e5fec7f01442887e
박연후 <qkrdusgn00@naver.com>
2024-09-09 17:27:32 +0900

    [init] BackEnd 폴더 생성

---
+++ back/heroin/src/main/java/lte/heroin/HeroinApplication.java
@@ -0,0 +1,13 @@
+package lte.heroin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeroinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeroinApplication.class, args);
+	}
+
+}
---
+++ back/heroin/src/test/java/lte/heroin/HeroinApplicationTests.java
@@ -0,0 +1,13 @@
+package lte.heroin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeroinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
