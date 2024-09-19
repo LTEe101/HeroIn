5fa0cbf3290ab885d8cad8e1e955872f6c74e0ce
박연후 <qkrdusgn00@naver.com>
2024-09-19 10:12:18 +0900

    [chore] 배포용 application 설정 추가

--- back/heroin/src/main/java/com/ssafy/Heroin/repository/HistoryCardRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ssafy.Heroin.repository;
-
-import com.ssafy.Heroin.domain.HistoryCard;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HistoryCardRepository extends JpaRepository<HistoryCard, Long> {
-}
