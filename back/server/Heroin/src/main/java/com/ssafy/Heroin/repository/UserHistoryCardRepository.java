fcef9d63220ee6c27c765ef64ec2bbc335e275cf
wonbin <jbs990609@naver.com>
2024-09-20 14:45:54 +0900

    [feat] 사용자 카드 정보 얻기 구현 및 프로필 정보 얻기 수정

--- back/server/Heroin/src/main/java/com/ssafy/Heroin/repository/UserHistoryCardRepository.java
+++ back/server/Heroin/src/main/java/com/ssafy/Heroin/repository/UserHistoryCardRepository.java
@@ -1,7 +1,6 @@
 package com.ssafy.Heroin.repository;
 
 import com.ssafy.Heroin.domain.UserHistoryCard;
-import com.ssafy.Heroin.dto.user.UserDto;
 import com.ssafy.Heroin.dto.user.UserHistoryCardDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -10,8 +9,6 @@ import java.util.List;
 
 public interface UserHistoryCardRepository extends JpaRepository<UserHistoryCard, Long> {
 
-    @Query("SELECT c " +
-            " FROM UserHistoryCard c" +
-            " WHERE (c.user.userId LIKE :#{#userDto.userLoginId})")
-    List<UserHistoryCardDto> findByUser(UserDto userDto);
+    @Query("SELECT c FROM UserHistoryCard c JOIN FETCH c.user u WHERE u.userId = :userId")
+    List<UserHistoryCard> findByUser(String userId);
 }
