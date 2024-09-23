228403e9cfadae69b60347549502d683b3409d01
이승지 <esg0107@gmail.com>
2024-09-23 11:33:00 +0900

    [refactor] 손 좌푯값 처리 코드 싱글톤 및 world position 기준으로 변경

--- unity/Heroin/Assets/HandTracking.cs
+++ unity/Heroin/Assets/HandTracking.cs
@@ -4,13 +4,27 @@ using UnityEngine;
 
 public class HandTracking : MonoBehaviour
 {
+    public static HandTracking Instance; // Singleton instance
+
     public UDPReceive udpReceive;
     public GameObject[] leftHandPoints;
     public GameObject[] rightHandPoints;
 
-    void Start()
+    public Vector3[] leftHandPositions = new Vector3[21]; // 저장된 좌표 데이터
+    public Vector3[] rightHandPositions = new Vector3[21]; // 저장된 좌표 데이터
+
+    void Awake()
     {
-        
+        // Singleton pattern
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
     void Update()
@@ -19,37 +33,43 @@ public class HandTracking : MonoBehaviour
         string leftdata = udpReceive.leftHandData;
         string rightdata = udpReceive.rightHandData;
 
-        // 데이터를 클리닝 (대괄호 제거)
-        leftdata = leftdata.Remove(0, 1);
-        leftdata = leftdata.Remove(leftdata.Length - 1, 1);
+        leftdata = CleanData(leftdata);
+        rightdata = CleanData(rightdata);
 
-        rightdata = rightdata.Remove(0, 1);
-        rightdata = rightdata.Remove(rightdata.Length - 1, 1);
-
-        // 쉼표를 기준으로 데이터를 분할
         string[] leftpoints = leftdata.Split(',');
         string[] rightpoints = rightdata.Split(',');
 
-        // 각 손의 좌표 데이터는 21개 점씩, 즉 63개의 값이 필요 (x, y, z).
+        if (leftpoints.Length != 63 || rightpoints.Length != 63)
+        {
+            Debug.LogWarning("Received data is not in expected format.");
+            return;
+        }
 
         // 왼손 데이터 처리
         for (int i = 0; i < 21; i++)
         {
-            float x = 5 - float.Parse(leftpoints[i * 3]) / 100;
+            float x = float.Parse(leftpoints[i * 3]) / 100;
             float y = float.Parse(leftpoints[i * 3 + 1]) / 100;
             float z = float.Parse(leftpoints[i * 3 + 2]) / 100;
 
-            leftHandPoints[i].transform.localPosition = new Vector3(x, y, z);
+            leftHandPositions[i] = new Vector3(x, y, z);
+            leftHandPoints[i].transform.position = new Vector3(x, y, z); // 위치를 worldPosition으로 설정
         }
 
         // 오른손 데이터 처리
         for (int i = 0; i < 21; i++)
         {
-            float x = 5 - float.Parse(rightpoints[i * 3]) / 100;
+            float x = float.Parse(rightpoints[i * 3]) / 100;
             float y = float.Parse(rightpoints[i * 3 + 1]) / 100;
             float z = float.Parse(rightpoints[i * 3 + 2]) / 100;
 
-            rightHandPoints[i].transform.localPosition = new Vector3(x, y, z);
+            rightHandPositions[i] = new Vector3(x, y, z);
+            rightHandPoints[i].transform.position = new Vector3(x, y, z); // 위치를 worldPosition으로 설정
         }
+    }
+
+    private string CleanData(string data)
+    {
+        return data.TrimStart('[').TrimEnd(']');
     }
 }
