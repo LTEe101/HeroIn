72f54efdb4b135e039329d891fe44ed8f1a19d2b
wonbin <jbs990609@naver.com>
2024-09-23 10:09:41 +0900

    [chore] erd 수정 및 title entity 수정

---
+++ back/server/Heroin/src/main/java/com/ssafy/Heroin/domain/UserTitle.java
@@ -0,0 +1,28 @@
+package com.ssafy.Heroin.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+public class UserTitle {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_title_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.EAGER, optional = false)
+    @JoinColumn(name = "usertable_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.EAGER, optional = false)
+    @JoinColumn(name = "title_id")
+    private Title title;
+
+    private LocalDateTime createAt;
+}
