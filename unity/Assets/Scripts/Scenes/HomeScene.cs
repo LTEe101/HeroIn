35be7c3da3725cfbbeefb8d12bac3fc7d45a0788
김태훈 <zxader@naver.com>
2024-09-25 13:58:56 +0900

    [feat] 타임머신 내부 버튼 효과 추가, 캐릭터 이동 추가, 스토리UI출력 변경

--- unity/Assets/Scripts/Scenes/HomeScene.cs
+++ unity/Assets/Scripts/Scenes/HomeScene.cs
@@ -1,20 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class HomeScene : BaseScene
 {
-    [SerializeField]
     private Vector3 _initialPosition = new Vector3(-10f, 1.910354f, -2.25f);
 
-    [SerializeField]
     private Quaternion _initialRotation = Quaternion.Euler(6f, 0.0f, 0.0f); // 초기 회전
 
-    [SerializeField]
     private Vector3 _targetPosition = new Vector3(-10.18889f, 2.267206f, -0.639335f); // 목표 위치
 
-    [SerializeField]
     private Quaternion _targetRotation = Quaternion.Euler(-1.082f, 0.104f, -0.001f); // 목표 회전
+
+    private Vector3 _originalScale; // 객체의 원래 Scale을 저장할 변수
+
+    private float _highlightScaleFactor = 1.02f; // 하이라이트 시 커질 비율
+
+    private GameObject _mainTV;
+    private TextMeshPro _storyYear;
+    private TextMeshPro _storyName;
+    private TextMeshPro _historicalFigure;
+    private Color _originalColor;
+    private GameObject _hitObject;
+    enum MonitorStatus
+    {
+        Close,
+        Story1592,
+        Story1919,
+    }
+
+    MonitorStatus _monitorStatus = MonitorStatus.Close;
 
     protected override void Init()
     {
@@ -22,8 +38,20 @@ public class HomeScene : BaseScene
 
         SceneType = Define.Scene.Home;
 
-        Managers.Input.MouseAction -= OnMouseClicked;
-        Managers.Input.MouseAction += OnMouseClicked;
+        Managers.Input.MouseAction -= OnMouseAction;
+        Managers.Input.MouseAction += OnMouseAction;
+
+        // main_tv 오브젝트를 찾거나 인스펙터에서 연결
+        if (_mainTV == null)
+        {
+            _mainTV = GameObject.Find("main_tv");
+            GameObject storyYearObject = Util.FindChild(_mainTV, "StoryYear");
+            GameObject storyNameObject = Util.FindChild(_mainTV, "StoryName");
+            GameObject historicalFigureObject = Util.FindChild(_mainTV, "HistoricalFigure");
+            _storyYear = storyYearObject.GetComponent<TextMeshPro>();
+            _storyName = storyNameObject.GetComponent<TextMeshPro>();
+            _historicalFigure = historicalFigureObject.GetComponent<TextMeshPro>();
+        }
 
         // Singleton 패턴으로 접근
         if (CameraController.Instance != null)
@@ -43,7 +71,7 @@ public class HomeScene : BaseScene
         // CameraController의 상태가 Move이거나 UI 스택이 존재하면 회전하지 않음
         if (CameraController.Instance.Status == CameraController.CameraStatus.Move || Managers.UI.GetStackCount() > 0) return;
 
-        HandleMouseRotation(); // 마우스 회전 처리
+        //HandleMouseRotation(); // 마우스 회전 처리
     }
 
     void HandleMouseRotation()
@@ -63,9 +91,26 @@ public class HomeScene : BaseScene
         // 필요 시 구현
     }
 
-    void OnMouseClicked(Define.MouseEvent evt)
+    // 마우스 입력 처리 메서드
+    void OnMouseAction(Define.MouseEvent mouseEvent)
     {
-        if (evt != Define.MouseEvent.Click) return;
+        switch (mouseEvent)
+        {
+            case Define.MouseEvent.Click:
+                OnMouseClicked();
+                break;
+            case Define.MouseEvent.PointerEnter:
+                OnPointerEnter(Managers.Input.HoveredObject); // 현재 호버된 객체 밝게 하기
+                break;
+            case Define.MouseEvent.PointerExit:
+                OnPointerExit(Managers.Input.HoveredObject); // 하이라이트 리셋
+                break;
+        }
+    }
+    
+    // 마우스 클릭 시 처리 메서드
+    void OnMouseClicked()
+    {
         if (CameraController.Instance == null) return;
 
         Ray ray = CameraController.Instance.GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
@@ -80,24 +125,29 @@ public class HomeScene : BaseScene
 
         if (Physics.Raycast(ray, out hit))
         {
-            string hitObjectName = hit.collider.gameObject.name;
-            Debug.Log(hitObjectName);
-            switch (hitObjectName)
+            if (_hitObject != null && _originalColor != default(Color))
+            {
+                Renderer renderer = _hitObject.GetComponent<Renderer>();
+                renderer.material.color = _originalColor;
+                _originalColor = default(Color); 
+            }
+
+            _hitObject = hit.collider.gameObject;
+
+            switch (_hitObject.name)
             {
                 case "UserButton":
+                    if (_monitorStatus != MonitorStatus.Close) CloseMonitory();
                     StartCoroutine(MoveCameraAndShowUI<UI_User>());
                     break;
                 case "1592":
-                    StartCoroutine(MoveToInitialPositionAndRotationAndShowUI<UI_Story_Info_1592>());
+                    OnStoryButton(MonitorStatus.Story1592, "1592", "한산도 대첩", "이순신 장군");
                     break;
                 case "1919":
-                    StartCoroutine(MoveToInitialPositionAndRotationAndShowUI<UI_Story_Info_1919>());
+                    OnStoryButton(MonitorStatus.Story1919, "1919", "3.1운동", "유관순 열사");
                     break;
                 case "Start":
-                    UI_Popup topPopup = Managers.UI.GetTopPopupUI();
-                    if (topPopup == null) break;
-                    // 이동
-                    LoadScene(topPopup);
+                    LoadScene();
                     break;
                 case "Exit":
                     Managers.Scene.LoadScene(Define.Scene.Museum);
@@ -110,6 +160,148 @@ public class HomeScene : BaseScene
         }
     }
 
+    // 버튼 컬러를 밝게 하는 메서드
+    void ChangeColor()
+    {
+        Renderer renderer = _hitObject.GetComponent<Renderer>();
+        _originalColor = renderer.material.color;
+        renderer.material.color = _originalColor * 1.3f;
+    }
+
+    // 스토리 버튼 메서드
+    void OnStoryButton(MonitorStatus status, string storyYear, string storyName, string historicalFigure)
+    {
+        if (_monitorStatus == status) // 화면에 출력되어 있는 화면 버튼 누른 경우
+        {
+            CloseMonitory();
+        }
+        else // 아닌 경우
+        {
+            ShowMonitor(status, storyYear, storyName, historicalFigure);
+            ChangeColor();
+        }
+    }
+
+    // 모니테 화면 On
+    void ShowMonitor(MonitorStatus status, string storyYear, string storyName, string historicalFigure)
+    {
+        _storyYear.text = storyYear;
+        _storyName.text = storyName;
+        _historicalFigure.text = historicalFigure;
+        _monitorStatus = status;
+    }
+
+    // 모니터 화면 Off
+    void CloseMonitory()
+    {
+        _storyYear.text = "";
+        _storyName.text = "";
+        _historicalFigure.text = "";
+        _monitorStatus = MonitorStatus.Close;
+    }
+
+    // 하이라이트를 원래 상태로 복구하는 함수
+    void ResetHighlight(GameObject hoveredObject)
+    {
+        if (hoveredObject != null)
+        {
+            // 원래 Scale로 복구
+            hoveredObject.transform.localScale = _originalScale;
+            hoveredObject = null; // 하이라이트 객체 초기화
+
+        }
+    }
+
+    void ChangeOutlineColor(GameObject go, Color newColor)
+    {
+        // 오브젝트에서 Outline 컴포넌트를 가져옴
+        Outline outline = go.GetComponent<Outline>();
+
+        // Outline 컴포넌트가 있으면 색상 변경
+        if (outline != null)
+        {
+            // 아웃라인 컬러 변경
+            outline.OutlineColor = newColor;
+        }
+        else
+        {
+            Debug.LogWarning("Outline 컴포넌트를 찾을 수 없습니다.");
+        }
+    }
+
+    void OnPointerEnter(GameObject hoveredObject)
+    {
+        if (hoveredObject == null) return;
+        switch (hoveredObject.name)
+        {
+            case "UserButton":
+                HighlightObject(hoveredObject);
+                ChangeOutlineColor(hoveredObject, Color.white);
+                break;
+            case "1592":
+                HighlightObject(hoveredObject);
+                ChangeOutlineColor(hoveredObject, Color.white);
+                break;
+            case "1919":
+                HighlightObject(hoveredObject);
+                ChangeOutlineColor(hoveredObject, Color.white);
+                break;
+            case "Start":
+                HighlightObject(hoveredObject);
+                ChangeOutlineColor(hoveredObject, Color.white);
+                break;
+            case "Exit":
+                HighlightObject(hoveredObject);
+                ChangeOutlineColor(hoveredObject, Color.white);
+                break;
+            default:
+                break;
+
+        }
+    }
+    void OnPointerExit(GameObject hoveredObject)
+    {
+        if (hoveredObject == null) return;
+        switch (hoveredObject.name)
+        {
+            case "UserButton":
+                ResetHighlight(hoveredObject);
+                ChangeOutlineColor(hoveredObject, Color.black);
+                break;
+            case "1592":
+                ResetHighlight(hoveredObject);
+                ChangeOutlineColor(hoveredObject, Color.black);
+                break;
+            case "1919":
+                ResetHighlight(hoveredObject);
+                ChangeOutlineColor(hoveredObject, Color.black);
+                break;
+            case "Start":
+                ResetHighlight(hoveredObject);
+                ChangeOutlineColor(hoveredObject, Color.black);
+                break;
+            case "Exit":
+                ResetHighlight(hoveredObject);
+                ChangeOutlineColor(hoveredObject, Color.black);
+                break;
+            default:
+                break;
+        }
+    }
+
+    // 마우스 호버 시 객체를 키우고 UI_Enter 출력하는 함수
+    void HighlightObject(GameObject hoveredObject)
+    {
+        if (hoveredObject != null)
+        {
+            // 원래 Scale 저장
+            _originalScale = hoveredObject.transform.localScale;
+
+            // 하이라이트 Scale 적용
+            hoveredObject.transform.localScale = _originalScale * _highlightScaleFactor; // Scale 증가
+        }
+    }
+
     // 카메라 위치와 회전을 변경하고 UI를 띄우는 메서드
     IEnumerator MoveCameraAndShowUI<T>() where T : UI_Popup
     {
@@ -118,47 +310,20 @@ public class HomeScene : BaseScene
         yield return StartCoroutine(CameraController.Instance.MoveToPositionAndRotation(_targetPosition, _targetRotation));
 
         // 이동이 완료된 후 UI 띄우기
-        ShowPopupUI<T>();
-    }
-
-    // 카메라 초기 위치와 회전으로 이동하고 UI를 띄우는 메서드
-    IEnumerator MoveToInitialPositionAndRotationAndShowUI<T>() where T : UI_Popup
-    {
-        // 카메라 이동
-        yield return StartCoroutine(CameraController.Instance.MoveToPositionAndRotation(_initialPosition, _initialRotation));
-
-        // 이동이 완료된 후 UI 띄우기
-        ShowPopupUI<T>();
-    }
-
-    public void ShowPopupUI<T>() where T : UI_Popup
-    {
-        UI_Popup topPopup = Managers.UI.GetTopPopupUI();
-        if (topPopup != null && topPopup.GetType() == typeof(T))
-        {
-            Managers.UI.CloseAllPopupUI();
-            return; // 이미 같은 팝업이 열려 있으면 아무 작업도 하지 않음
-        }
-        else if (Managers.UI.GetStackCount() > 0)
-        {
-            Managers.UI.CloseAllPopupUI();
-        }
         Managers.UI.ShowPopupUI<T>();
     }
 
-    public void LoadScene(UI_Popup topPopup)
+    public void LoadScene()
     {
-        if (topPopup.GetType() == typeof(UI_Story_Info_1592))
+        switch (_monitorStatus)
         {
-            //Managers.Scene.LoadingLoadScene(Define.Scene.Game);
-            // SceneManager.LoadScene();
-            Debug.Log("이동");
+            case MonitorStatus.Story1592:
+                ChangeColor();
+                //Managers.Scene.LoadingLoadScene(Define.Scene.Game);
+                Debug.Log("이동");
+                break;
+            default:
+                break;
         }
-    }
-
-    // 카메라를 원래 위치과 회전으로 돌아가게 하는 메서드
-    public void MoveToInitialPositionAndRotation()
-    {
-        StartCoroutine(CameraController.Instance.MoveToPositionAndRotation(_initialPosition, _initialRotation));
     }
 }
