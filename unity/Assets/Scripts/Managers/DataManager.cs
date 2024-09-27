db3739d65a751a2d047d1a64ecbf3f6ed7cdb0e5
이승지 <esg0107@gmail.com>
2024-09-27 11:10:15 +0900

    [feat] 모션 게임 튜토리얼 1차 완성

--- unity/Assets/Scripts/Managers/DataManager.cs
+++ unity/Assets/Scripts/Managers/DataManager.cs
@@ -20,6 +20,13 @@ public class DataManager
     {
         CardDict = LoadJson<HistoryCardData, int, HistoryCard>("HistoryCardData").MakeDict();
         NarrationData narrationData = LoadJson<NarrationData>("NarrationData");
+        GameInfos = LoadJson<GameInfoData>("GameInfoData").MakeDict();
+        Debug.Log($"Loaded GameInfos count: {GameInfos.Count}");
+        foreach (var info in GameInfos)
+        {
+            Debug.Log($"Game ID: {info.Key}, Title: {info.Value.title}");
+        }
+
         BeforeStoryList = narrationData.beforeStory;
         AfterStoryList = narrationData.afterStory;
 
